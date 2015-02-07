0baea5f65b453c100fc9b0c98752f0ad9eecba72
Kevin O'Toole <kotoole@onshape.com>
2015-02-07 17:52:43 -0500

    putting analog read on an interrupt

--- joystick/analogRead.c
+++ joystick/analogRead.c
@@ -7,16 +7,25 @@
 
 uint8_t string[40];
 
+volatile uint16_t pin5;
+
+void read_pins(_TIMER *self) {
+    pin5 = pin_read(&A[5]) >> 6;
+}
+
 int16_t main(void) {
     init_clock();
     init_pin();
     init_timer();
     init_uart();
 
+    timer_every(&timer2, 0.01, read_pins);
+
     printf("Analog Read code: \n");
 
-    timer_setPeriod(&timer1, 0.05);
+    timer_setPeriod(&timer1, 0.1);
     timer_start(&timer1);
+
 
     int i;
     for (i = 0; i<6; i++) {
@@ -24,9 +33,13 @@ int16_t main(void) {
     }
 
     while (1) {
+        // printf(">> ");
+        // uart_gets(&uart1, string, 40);
+        // printf("You typed '%s'\n", string);
+
         if (timer_flag(&timer1)) {
             timer_lower(&timer1);
-            printf("%d\n", pin_read(&A[5]) >> 6);
+            printf("%d\n", pin5);
         }
     }
 }
