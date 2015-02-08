a53b3f693d02ad2c06c71ebeea2cbdc8913c6008
Kevin O'Toole <kotoole@onshape.com>
2015-02-08 14:09:33 -0500

    reading pins and setting state on separate timers

--- joystick/helloMotor.c
+++ joystick/helloMotor.c
@@ -5,6 +5,7 @@
 #include "timer.h"
 #include "pin.h"
 #include "oc.h"
+#include "uart.h"
 
 volatile uint16_t pin5;
 volatile uint16_t switch1;
@@ -12,46 +13,66 @@ volatile uint16_t switch1;
 typedef enum {
     ON,
     PWM,
-    OFF
+    OFF,
+    FREE
 } MotorState;
 
-MotorState motor_state = OFF;
-
-void pwm_motor(void)
-{
-    oc_pwm(&oc1, &D[4], NULL, 500, 1023 << 6);
-}
+MotorState motor_state = FREE;
 
 void stop_motor() {
     pin_clear(&D[5]);
     pin_clear(&D[6]);
 }
 
-void read_pins(_TIMER *self) {
+void start_motor_clockwise() {
+    pin_clear(&D[3]);
+    pin_set(&D[4]);
+    pin_set(&D[5]);
+}
+
+void freespin_motor() {
+    pin_set(&D[3]);
+    pin_clear(&D[4]);
+    stop_motor();
+}
+
+void pwm_motor(void)
+{
+    start_motor_clockwise();
+    oc_pwm(&oc1, &D[4], NULL, 500, 1023 << 6);
+}
+
+void read_pins() {
     pin5 = pin_read(&A[5]) >> 6;
+}
+
+void switch_state() {
     switch1 = sw_read(&sw1);
-    if (motor_state == OFF) {
-        if (switch1) {
+    printf("%d\n", motor_state);
+    if (motor_state == FREE) {
+        if (!switch1) {
             motor_state = PWM;
             pwm_motor();
         }
     }
     if (motor_state == PWM)
     {
-        if (!switch1)
+        if (switch1)
         {
-            motor_state = OFF;
-            stop_motor();
+            motor_state = FREE;
+            freespin_motor();
         }
     }
 }
 
-void start_motor_clockwise() {
-    pin_set(&D[5]);
+void setup_pins() {
+    int i;
+    for (i = 0; i<6; i++) {
+        pin_analogIn(&A[i]);
+    }
 }
 
 void setup_motor() {
-    init_pin();
     pin_digitalOut(&D[2]);
     pin_digitalOut(&D[3]);
     pin_digitalOut(&D[4]);
@@ -64,22 +85,29 @@ void setup_motor() {
 }
 
 int16_t main(void) {
-    setup_motor();
+    init_pin();
     init_clock();
     init_ui();
+    init_uart();
     init_timer();
     init_oc();
+    setup_pins();
+    setup_motor();
 
-    timer_every(&timer2, 0.001, read_pins);
+    timer_setPeriod(&timer1, 0.0001);
+    timer_start(&timer1);
+    timer_setPeriod(&timer2, 0.01);
+    timer_start(&timer2);
+
 
     while (1) {
-        // if (switch1)
-        // {
-        //     oc_pwm(&oc1, &D[4], NULL, 500, 1023 << 6);
-        // }
-        // else
-        // {
-        //     pin_clear(&D[4]);
-        // }
+        if (timer_flag(&timer1)) {
+            timer_lower(&timer1);
+            read_pins();
+        }
+        if (timer_flag(&timer2)) {
+            timer_lower(&timer2);
+            switch_state();
+        }
     }
 }
