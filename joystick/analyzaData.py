bedef8fe725faa80ddde6f148e5fd97fee8f4235
Kevin O'Toole <kotoole@onshape.com>
2015-02-08 20:34:25 -0500

    spin down tests, adding new data

--- joystick/analyzaData.py
+++ joystick/analyzaData.py
@@ -8,21 +8,27 @@ if platform.system() == 'Linux':
 elif platform.system() == 'Darwin':
 	ser = serial.Serial('/dev/tty.usbserial-FTGR7A7L', 19200, timeout=1)
 
+timeConstant = 15
+
 def getData():
 	values = []
 	startTime = time.time()
+	print "started serial read"
 	while 1:
 		line = ser.readline()   # read a '\n' terminated line
-		if (line != ''):
-			if int(line) < 800 and int(line) > 0:
+		try:
+			if int(line) < 600 and int(line) > 0:
 				values.append(int(line))
+		except:
+			print line
 		if time.time() - startTime > 3:
 			break 
+	print "Finished gathering data"
 	ser.close()
 	return values
 
 def processData(data):
-	timeConstant = 5
+	data.remove(max(data))
 	samplingTime = .0001 #seconds
 	total = 0
 	times = []
@@ -31,26 +37,26 @@ def processData(data):
 		total += time
 		times.append(total)
 
-	speeds = [1.0/max(times[i:i+timeConstant]) for i in xrange(len(times) - timeConstant)]
+	speeds = [1.0/(max(data[i:i+timeConstant]) * samplingTime) 
+		for i in xrange(len(data) - timeConstant)]
 	return times, speeds
 
 def makePlots(times, speeds, plotType):
 	if plotType == "spin-down":
-		plt.plot(times, values)
-		plt.ylabel("Cycles Between Flips")
-		plt.xlabel("Flip Value")
+		plt.plot(times[:-timeConstant], speeds)
+		plt.ylabel("Speed of motor (rotations per second)")
+		plt.xlabel("Time (s)")
+		plt.title("Spin Down Test", fontsize="28")
 
 	elif plotType == "calibration":
 		plt.plot(values)
 		plt.ylabel("Voltage")
 		plt.xlabel("Angle")
-	# plt.title(plotType)
-	# plt.show()
-
+	plt.show()
 
 if __name__ == "__main__":
 
 	values = getData()
-	times, speed = processData(values)
+	times, speeds = processData(values)
 	makePlots(times, speeds, 'spin-down')
 
